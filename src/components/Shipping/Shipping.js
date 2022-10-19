21b004387bd9c275ef1d7e097d6b639c5cc965d7
GaziAshiq <ashiq121914@gmail.com>
2022-10-19 10:59:21 +0600

    project done

---
+++ src/components/Shipping/Shipping.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Shipping = () => {
+  return (
+    <div>
+      <h3>this is shipping</h3>
+      <h3>delivery on the way</h3>
+      <p>coming soon...</p>
+    </div>
+  );
+};
+
+export default Shipping;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMnG8i7XZdZGRqCCJYf5tj8YI5WU-ElLI",
+  authDomain: "ema-john-simple-e17f1.firebaseapp.com",
+  projectId: "ema-john-simple-e17f1",
+  storageBucket: "ema-john-simple-e17f1.appspot.com",
+  messagingSenderId: "397330906603",
+  appId: "1:397330906603:web:219cbd309db481d374edae",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
